b69f9cc3825495b749e1043545a70e08e5d738e0
sowjanya <90475451+simplysowj@users.noreply.github.com>
2024-01-26 07:40:54 -0500

    Update constant.py

--- portfolio-template-LLM/constant.py
+++ portfolio-template-LLM/constant.py
@@ -2,7 +2,7 @@ info = {
    "Pronoun": "her", 
    "Name": "Sowjanya",
    "Full_Name": "Sowjanya Bojja",
-   "Intro": "Data Scienetist",
+   "Intro": "Data Scientist",
    "About":"Hey there, I'm Sowjanya! I'm passionate about leveraging data and technology to drive meaningful insights and solutions in business.",
    "Github":"https://github.com/simplysowj",
    "Linkedin":"https://www.linkedin.com/in/sowjanya-bojja/",
