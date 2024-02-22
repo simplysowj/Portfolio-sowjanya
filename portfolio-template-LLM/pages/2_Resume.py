692e946ee3a7a8654f6b4e0c1b2eb146373fb7bc
sowjanya <90475451+simplysowj@users.noreply.github.com>
2024-02-21 23:13:40 -0500

    Update 2_Resume.py

--- portfolio-template-LLM/pages/2_Resume.py
+++ portfolio-template-LLM/pages/2_Resume.py
@@ -14,7 +14,7 @@ st.title("📝 Resume")
 
 
 
-with open("portfolio-template-LLM/images/Data_Scientist_Resume_Sowjanya_with_projects.pdf","rb") as f:
+with open("portfolio-template-LLM/images/resume.pdf","rb") as f:
       base64_pdf = base64.b64encode(f.read()).decode('utf-8')
       pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1000mm" height="1000mm" type="application/pdf"></iframe>'
       st.markdown(pdf_display, unsafe_allow_html=True)
