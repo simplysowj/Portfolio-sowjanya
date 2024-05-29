508cc77228d64e9f27a5afcae1ac5442381750fa
sowjanya <90475451+simplysowj@users.noreply.github.com>
2024-05-29 18:39:04 -0400

    Update ResumeinWindows.py

--- portfolio-template-LLM/pages/ResumeinWindows.py
+++ portfolio-template-LLM/pages/ResumeinWindows.py
@@ -1,26 +1,28 @@
 import streamlit as st
+from PIL import Image
+from constant import *
 
-#st.sidebar.markdown(info['Photo'], unsafe_allow_html=True)
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+        
+local_css("portfolio-template-LLM/style/style.css")
 
-st.title("📝 Resume")
+st.sidebar.markdown(info['Photo'],unsafe_allow_html=True)
 
-# Path to the PDF file
-pdf_path = "portfolio-template-LLM/images/Sowjanya_Data_science_latest_resume.pdf"
+img_1 = Image.open("portfolio-template-LLM/images/1.jpg")
+img_2 = Image.open("portfolio-template-LLM/images/2.png")
+img_3 = Image.open("portfolio-template-LLM/images/3.png")
 
-# Provide a download button for the PDF
-with open(pdf_path, "rb") as pdf_file:
-    pdf_bytes = pdf_file.read()
-    st.download_button(
-        label="Download Resume as PDF",
-        data=pdf_bytes,
-        file_name="Sowjanya_Data_science_latest_resume.pdf",
-        mime="application/pdf"
-    )
+st.title("🫶 Hobbies")
 
-# Inform users about the download button
-st.markdown(
-    """
-    Click the button above to download the resume as a PDF file.
-    """,
-    unsafe_allow_html=True
-)
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.image(img_1)
+   
+with col2:
+   st.image(img_2)
+
+with col3:
+   st.image(img_3)
