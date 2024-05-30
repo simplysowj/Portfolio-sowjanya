f0fc3eee4abe9aac4b8fbaaaa8d76532f6918b23
sowjanya <90475451+simplysowj@users.noreply.github.com>
2024-05-29 21:56:25 -0400

    Update 1_🏠_Home.py

--- portfolio-template-LLM/1_🏠_Home.py
+++ portfolio-template-LLM/1_🏠_Home.py
@@ -157,13 +157,13 @@ with st.container():
     with col4:
         st_lottie(lottie_animations['react'], height=50, width=50, key="react")
     with col1:
-            st.image(img_1 ,height=50)
+            st.image(img_1 )
     with col2:
-            st.image(img_2 ,width=50, height=50)
+            st.image(img_2 )
     with col3:
-            st.image(img_3 ,width=50, height=50)
+            st.image(img_3 )
     with col4:
-            st.image(img_4 ,width=50, height=50)
+            st.image(img_4 )
     
 
     # Technical Skills Section
