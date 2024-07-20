22dc6e7c301495418328c0f36ab0f5fe452f34c7
sowjanya <90475451+simplysowj@users.noreply.github.com>
2024-07-20 09:49:28 -0400

    Update 6_info.py

--- portfolio-template-LLM/pages/6_info.py
+++ portfolio-template-LLM/pages/6_info.py
@@ -1,89 +1,106 @@
 import streamlit as st
-from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
-from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.output_parsers import StrOutputParser
-from langchain_community.document_loaders import WebBaseLoader
-from langchain_text_splitters import NLTKTextSplitter
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain_community.vectorstores import Chroma
-from langchain_core.runnables import RunnablePassthrough
-from IPython.display import Markdown as md
-from langchain_community.document_loaders import PyPDFLoader
-st.title("🤖 RAG System on personal portfolio 📄")
-user_input = st.text_input("Enter text ....")
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
 
-chat_model = ChatGoogleGenerativeAI(google_api_key="AIzaSyDvU3SwP_TMwEv_pfyy9JIqE_BMm4Y5O0Q", 
-                                   model="gemini-1.5-pro-latest")
-
-
-chat_template = ChatPromptTemplate.from_messages([
-    # System Message Prompt Template
-    SystemMessage(content="""You are a Helpful AI Bot. 
-    You take the question from user and answer if you have the specific information related to the question. """),
-    # Human Message Prompt Templatestrea
-    HumanMessagePromptTemplate.from_template("""Aswer the following question: {question}
-    Answer: """)
-])
+load_dotenv()
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
-output_parser = StrOutputParser()
-
-#Loading Document
-loader = PyPDFLoader(r"portfolio-template-LLM/images/Data_Scientist_Resume_Sowjanya_with_projects.pdf")
-pages = loader.load_and_split()
-
-data = loader.load()
-
-#splitting documents into chunkers
-
-text_splitter = NLTKTextSplitter(chunk_size=500, chunk_overlap=100)
-
-chunks = text_splitter.split_documents(data)
-# Creating Chunks Embedding
-embedding_model = GoogleGenerativeAIEmbeddings(google_api_key="AIzaSyDvU3SwP_TMwEv_pfyy9JIqE_BMm4Y5O0Q", 
-                                               model="models/embedding-001")
-
-# Store the chunks in vector store
-db = Chroma.from_documents(chunks, embedding_model, persist_directory="./chroma_db_")
-db.persist()
-# Setting a Connection with the ChromaDB
-db_connection = Chroma(persist_directory="./chroma_db_", embedding_function=embedding_model)
-# Converting CHROMA db_connection to Retriever Object
-retriever = db_connection.as_retriever(search_kwargs={"k": 5})
-#retrieved_docs = retriever.invoke(user_input)
-
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
 
 
-chat_template = ChatPromptTemplate.from_messages([
-    # System Message Prompt Template
-    SystemMessage(content="""You are a Helpful AI Bot. 
-    You take the context and question from user. Your answer should be based on the specific context."""),
-    # Human Message Prompt Template
-    HumanMessagePromptTemplate.from_template("""Aswer the question based on the given context.
-    Context:
-    {context}
+
+
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+    return  text
+
+
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+
+def get_conversational_chain():
+
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+
+    model = ChatGoogleGenerativeAI(model="gemini-pro",
+                             temperature=0.3)
+
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+    return chain
+
+
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     
-    Question: 
-    {question}
+    new_db = FAISS.load_local("faiss_index", embeddings)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+
     
-    Answer: """)
-])
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
+    response = chain(
+        {"input_documents":docs, "question": user_question}
+        , return_only_outputs=True)
 
-
-rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | chat_template
-    | chat_model
-    | output_parser
-)
+    print(response)
+    st.write("Reply: ", response["output_text"])
 
 
 
-if st.button("Genrate"):
-    response = rag_chain.invoke(user_input)
-    st.write(response)
+
+def main():
+    st.set_page_config("Chat PDF")
+    st.header("Chat with PDF using Gemini💁")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    if user_question:
+        user_input(user_question)
+
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
+
+
+
+if __name__ == "__main__":
+    main()
