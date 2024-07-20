7bfe3a7a8f2d295881f8aeeeca10cb4b66f2de0f
sowjanya <90475451+simplysowj@users.noreply.github.com>
2024-07-20 09:51:10 -0400

    Update 6_info.py

--- portfolio-template-LLM/pages/6_info.py
+++ portfolio-template-LLM/pages/6_info.py
@@ -10,9 +10,8 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
-load_dotenv()
-os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+genai.configure(api_key="AIzaSyAIELlvvXlhVvwYnhFw-Bi62ohDeG68-RY")
 
 
 
