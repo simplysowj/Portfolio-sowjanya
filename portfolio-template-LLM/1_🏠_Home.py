42163c0e367363e49e5c315b7204c436828414d7
sowjanya <90475451+simplysowj@users.noreply.github.com>
2024-07-20 23:43:27 -0400

    Update 1_🏠_Home.py

--- portfolio-template-LLM/1_🏠_Home.py
+++ portfolio-template-LLM/1_🏠_Home.py
@@ -21,7 +21,7 @@ img_3=img_3.resize((70, 70))
 st.set_page_config(page_title='Template' ,layout="wide",page_icon='👧🏻')
 
 
-pdf_path = "portfolio-template-LLM/images/Sowjanya_Data_science_latest_resume.pdf"
+pdf_path = "portfolio-template-LLM/images/Resume_Sowjanya_DataScience_New.pdf"
 
 
 
